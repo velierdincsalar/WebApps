2ff2b57e57991fb8d0ebb320f7dec43c805bf5ec
Veli Erdinç Salar <velierdincsalar@gmail.com>
2022-05-08 23:14:56 +0300

    start new php script
    start

--- libs/wep.php
+++ libs/wep.php
@@ -1,12 +1,12 @@
 <?php
 
 
-/*
+
 $router->get('/asd', function() {
     return 'Hello World!';
 });
-*/
-//$router->get('/admin/{s}/{u}', 'Admin.HomeController@index');
+
+$router->get('/admin/{s}/{u}', 'Admin.HomeController@index');
 $router->get('/admin', 'Admin.HomeController@index');
 $router->get('/install', 'InstallWepApps@index');
 $router->get('/install/table', 'InstallWepApps@index2');
